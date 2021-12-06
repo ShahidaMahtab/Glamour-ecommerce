f54de3567e5a9b8ff481ad44d9ba05e307b3fd2c
jannat56 <jannatnishat30@gmail.com>
2021-12-06 23:29:05 +0600

    product

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,20 +1,22 @@
-import React from "react";
-import Footer from "../../Shared/Footer/Footer";
-import Navigation from "../../Shared/Navigation/Navigation";
-import Features from "../Features/Features";
-import About from "../About/About";
-import Banner from "../Banner/Banner";
+import React from 'react';
+import Footer from '../../Shared/Footer/Footer';
+import Navigation from '../../Shared/Navigation/Navigation';
+import Features from '../Features/Features';
+import About from '../About/About';
+import Banner from '../Banner/Banner';
+import Products from '../Products/Products';
 
 const Home = () => {
-  return (
-    <div>
-      <Navigation />
-      <Banner/>
-      <Features/>
-      <About/>
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Navigation />
+			<Banner />
+			<Features />
+			<About />
+			<Products />
+			<Footer />
+		</div>
+	);
 };
 
 export default Home;
---
+++ src/Pages/Home/Product/Product.js
@@ -0,0 +1,63 @@
+import { AddShoppingCart } from '@mui/icons-material';
+import {
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Grid,
+	IconButton,
+	Typography,
+} from '@mui/material';
+import React from 'react';
+
+const Product = ({ product }) => {
+	const { img, name, price, description } = product;
+	return (
+		<Grid item xs={12} md={4}>
+			<Card sx={{ maxWidth: 345, background: '#202020' }}>
+				<CardMedia
+					component='img'
+					height='140'
+					image={img}
+					alt='green iguana'
+					title={name}
+				/>
+				<CardContent sx={{ height: '100px' }}>
+					<div className='flex justify-between'>
+						<Typography
+							gutterBottom
+							variant='h6'
+							component='div'
+							color='white'
+							className='uppercase font-bold'
+						>
+							{name}
+						</Typography>
+						<Typography variant='h6' color='white'>
+							${price}
+						</Typography>
+					</div>
+					<Typography
+						variant='body2'
+						color='white'
+						sx={{ textAlign: 'left' }}
+					>
+						{description.slice(0, 50)}...
+					</Typography>
+				</CardContent>
+
+				<CardActions>
+					<IconButton
+						aria-label='add to cart'
+						sx={{ color: 'white', display: 'flex', ml: 'auto' }}
+					>
+						<AddShoppingCart />
+					</IconButton>
+				</CardActions>
+			</Card>
+		</Grid>
+	);
+};
+
+export default Product;
